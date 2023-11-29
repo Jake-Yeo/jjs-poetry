48af1a1707f6a07812aa62dc85189689f62c9992
Jake Yeo <jyeo03@students.cs.ubc.ca>
2023-11-29 04:10:11 -0800

    Added Sun Animation

---
+++ src/components/SunsetBackground.tsx
@@ -0,0 +1,97 @@
+import { Box, BoxProps } from "@mui/material";
+import { keyframes } from '@emotion/react';
+
+const moveSunUpAnimation = keyframes`
+  0% {
+    transform: translateY(50%); // Start from the bottom
+  }
+  100% {
+    transform: translateY(-2%); // Move to the top
+  }
+`;
+
+
+export const SunsetBackground = (props: React.PropsWithChildren & BoxProps) => {
+  return (
+    // Wrap the entire animation inside of another box that will act as the background
+    <Box
+      sx={{
+        zIndex: 1,
+        position: 'absolute',
+        width: '100vw', // Fixed viewport width
+        height: '100vh', // Fixed viewport height
+        overflow: 'hidden', // Prevent scrolling caused by overflowing content
+        ...props.sx
+      }}
+    >
+
+      {/*Full image without the moon*/}
+      <Box
+        sx={{
+          zIndex: 1,
+          position: 'absolute',
+          width: '100vw', // Fixed viewport width
+          height: '100vh', // Fixed viewport height
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundImage: 'url("/SunsetVectorNoSun.svg")',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center center',
+          ...props.sx
+        }}
+        {...props}
+      >
+
+        {/* Moving Sun */}
+        <Box
+          sx={{
+            zIndex: 3,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundImage: 'url("/SunsetVectorSunOnly.svg")',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center center',
+            animation: `${moveSunUpAnimation} 5s forwards`
+          }}
+        />
+        {/* Mountains only image to cover the moon*/}
+        <Box
+          sx={{
+            zIndex: 4,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundImage: 'url("/SunsetVectorBlackCloudsOnly.svg")',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center center'
+          }}
+        />
+        {/* Mountains only image to cover the sign in button*/}
+        <Box
+          sx={{
+            zIndex: 7,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundImage: 'url("/WinterVectorTransparentBackgroundWithSnow.svg")',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center center',
+          }}
+        />
+        {props.children}
+      </Box>
+    </Box>
+  );
+}
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,17 @@
+import { Typography } from '@mui/material';
+import { SunsetBackground } from '../components/SunsetBackground';
+
+export const MainPage = () => {
+
+    
+
+    return (<>
+        <SunsetBackground>
+            <Typography variant="h1">
+                Sunset Poetry
+            </Typography >
+        </SunsetBackground>
+        </>
+    );
+
+};
