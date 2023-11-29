864f99f3d97fb289ef1185d06f5ca99303aa45d3
Jake Yeo <jyeo03@students.cs.ubc.ca>
2023-11-29 07:19:31 -0800

    Made formatting better

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,35 @@
 import { Box, Grid, Typography, keyframes } from '@mui/material';
 import { SunsetBackground } from '../components/SunsetBackground';
+import { useEffect } from 'react';
 
 export const MainPage = () => {
+
+    useEffect(() => {
+        // Hide scrollbar but allow scrolling for Chrome, Safari, and Opera
+        document.body.style.overflow = 'auto';
+    
+        // For Chrome, Safari, and Opera
+        const styleElement = document.createElement('style');
+        styleElement.appendChild(document.createTextNode(`
+            ::-webkit-scrollbar {
+                display: none;
+            }
+        `));
+        document.head.appendChild(styleElement);
+    
+        // Re-enable default scrollbars when the component is unmounted
+        return () => {
+            document.body.style.overflow = 'unset';
+            document.head.removeChild(styleElement);
+        };
+    }, []);
 
     const moveTextUpAnimation = keyframes`
   0% {
     transform: translateY(500vh); // Start from the bottom
   }
   100% {
-    transform: translateY(0vh); // Move to the top
+    transform: translateY(-25vh); // Move to the top
   }
 `;
 
@@ -18,37 +39,47 @@ export const MainPage = () => {
         fontWeight: 'bold',
         fontSize: '3rem',
         textAlign: 'center',
-        animation: `${moveTextUpAnimation} 5s forwards`,
+    };
+
+    const authorTextStyle = {
+        fontFamily: 'Montserrat, sans-serif',
+        color: '#FDFACD',
+        fontWeight: 'bold',
+        fontSize: '2rem',
+        textAlign: 'center',
     };
 
     return (<>
-        <SunsetBackground>
-            <Grid
-                container
-                direction="column"
-                alignItems="baseline">
-                <Grid item xs={12} sx={{
+        <Grid
+            container
+            direction="column"
+            justifyContent="flex-start">
+            <Grid item sx={{
+                width: '100vw',
+                height: '100vh',
+            }}>
+                <SunsetBackground>
+                    <Box sx={{animation: `${moveTextUpAnimation} 5s forwards`,}}>
+                        <Typography variant="h1" sx={headerStyle}>
+                            Sunset Poetry
+                        </Typography >
+                        <Typography variant="h2" sx={authorTextStyle}>
+                            By: Jake Yeo
+                        </Typography >
+                    </Box>
+                </SunsetBackground>
+            </Grid>
+            <Grid item>
+                <Box sx={{
                     width: '100vw',
                     height: '100vh',
+                    color: '#FDFACD',
+                    background: 'blue'
                 }}>
-                    <Typography variant="h1" sx={headerStyle}>
-                        Sunset Poetry
-                    </Typography >
-                </Grid>
-                <Grid item xs={12} sx={{ padding: 0 }}>
-                    <Box sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        width: '100vw',
-                        height: '100vh',
-                        color: '#FDFACD',
-                        background: 'blue'
-                    }}>
-                        aweiufuawerghaiuerg
-                    </Box>
-                </Grid>
+                    aweiufuawerghaiuerg
+                </Box>
             </Grid>
-        </SunsetBackground>
+        </Grid>
     </>
     );
 
